695cee8f60d42da830a8cb1ba6e374d0ac4acee3
Danir-Pires <dro.pires@campus.fct.unl.pt>
2024-10-25 11:38:50 +0100

    Add files via upload

---
+++ Cubesat_pitch_dynamics1.m
@@ -0,0 +1,20 @@
+
+
+try %octave only
+  pkg load control
+end
+
+b_z = 22960.2*10^-9; %Campo Magnético que passa pelo cubo
+J_y =1.667*10^-3; %Momento de inércia segundo o eixo dos yy;
+a = b_z/J_y; %Constante
+s = tf('s');
+G = a/(s^2); %Função transferência a analisar
+
+%1.3
+figure (1);
+bode(G); %Diagrama de Bode da função tranferência
+grid on;
+
+%1.5
+figure (2);
+step(G,100); %Step response da função transferência
