1f3914edd851e89d47562013a6194f5c1b8f2f6a
Danir-Pires <dro.pires@campus.fct.unl.pt>
2025-01-04 17:48:31 +0000

    Add files via upload

---
+++ Lab3/Non_Linear_System.m
@@ -0,0 +1,100 @@
+clear 
+close all
+clc
+
+% Parâmetros
+g_mars = 3.721;
+Cd = 1.05;
+ro_mars = 0.02;
+z_I = [0;0;1];
+
+%Para o Lander
+l_L = 3.0;
+w_L = 2.7;
+h_L = 2.2;
+m = 1025; %Massa do lander é igual à massa do Rover
+mt = 2*m;
+J_L = [(1/12)*m*(l_L^2+h_L^2) 0 0  %Momento de Inércia do Lander
+      0 (1/12)*m*(w_L^2+h_L^2) 0   
+      0 0 (1/12)*m*(l_L^2+w_L^2)];
+
+%Para o Rover
+l_R = 2.7;
+w_R = 3.0;
+h_R = 2.2;
+
+J_R = [(1/12)*m*(l_R^2+h_R^2) 0 0  %Momento de Inércia do Rover
+      0 (1/12)*m*(w_R^2+h_R^2) 0
+      0 0 (1/12)*m*(l_R^2+w_R^2)];
+
+J = [J_L(1,1)+m*(1.1)^2+J_R(1,1)+m*(1.1)^2 0 0  %Momento de Inércia do Sistema
+     0 J_L(2,2)+m*(1.1)^2+J_R(2,2)+m*(1.1)^2 0
+     0 0 J_L(3,3)+J_R(3,3)];
+
+%Posições dos retrorockets
+p1 = [-1.35;-1.5;0];
+p2 = [1.35;-1.5;0];
+p3 = [1.35;1.5;0];
+p4 = [-1.35;1.5;0];
+
+%Thrust de cada retrorocket
+%Como T1=T2=T3=T4 podemos admitir um T genérico tal que Tt=4*T
+T = m*g_mars*0.25/cos(deg2rad(30));
+
+%Vetor da Força Propulsiva
+f1 = [0;sin(deg2rad(30));-cos(deg2rad(30))];
+f2 = [0;sin(deg2rad(30));-cos(deg2rad(30))];
+f3 = [0;sin(deg2rad(-30));-cos(deg2rad(-30))];
+f4 = [0;sin(deg2rad(-30));-cos(deg2rad(-30))];
+fp = T*f1+T*f2+T*f3+T*f4;
+
+
+%Vetor de Momentos 
+n1 = cross(p1,f1);
+n2 = cross(p2,f2);
+n3 = cross(p3,f3);
+n4 = cross(p4,f4);
+np = T*n1+T*n2+T*n3+T*n4;
+
+% Simulação do Sistema não Linear
+%Parâmetros
+Dt = 0.01;
+t = 0:Dt:60; %visto a fase de powered descend demorar aproximadamente 1 minuto
+u = [fp;np]*ones(size(t)) - 0.5*[fp;np]*(t>=35);
+nx = 12;
+ny = 4;
+x0 = [0;0;-2100;0;0;89;0;0;0;0;0;0];
+Nsim = length(t);
+x = zeros(nx,Nsim);
+y = zeros(ny,Nsim);
+x(:,1) = x0;
+
+%Simulação
+for k = 1:Nsim
+
+    p = x(1:3,k);
+    v = x(4:6,k);
+    lbd = x(7:9,k);
+    omega = x(10:12,k);
+    R = Euler2R(lbd);
+    Q = Euler2Q(lbd);
+    fg = g_mars*R'*z_I;
+    fa = -0.5*ro_mars*l_L*l_L*z_I;
+    fp = u (1:3,k);
+    np = u (4:6,k);
+
+    p_dot = R*v;
+    v_dot = -skew(omega)*v+1/mt*(fg+fa*v(3)^2+fp);
+    lbd_dot = Q*omega;
+    omega_dot = (-J^-1*skew(omega)*J*omega)+(J^-1*np);
+    x_dot = [p_dot;v_dot;lbd_dot;omega_dot];
+
+    x(:,k+1) = x(:,k) + Dt*x_dot;
+  
+end
+x(:,k+1) = [];
+
+folder = '..\figures\';
+example_name = 'ex4-7_rocket3d_sim';
+printfigs = 0;
+vehicle3d_show_data(t,x,u,0,[folder example_name],90270);
---
+++ Lab3/sim_linear_4.m
@@ -0,0 +1,266 @@
+close all;
+clear;
+% Parâmetros
+g_mars = 3.721;
+Cd = 1.05;
+ro_mars = 0.02;
+z_I = [0;0;1];
+
+%Para o Lander
+l_L = 3.0;
+w_L = 2.7;
+h_L = 2.2;
+m = 1025; %Massa do lander é igual à massa do Rover
+mt = 2*m;
+J = [(1/12)*m*(l_L^2+h_L^2) 0 0  %Momento de Inércia do Lander
+      0 (1/12)*m*(w_L^2+h_L^2) 0   
+      0 0 (1/12)*m*(l_L^2+w_L^2)];
+
+%Posições dos retrorockets
+p1 = [-1.35;-1.5;1.1];
+p2 = [1.35;-1.5;1.1];
+p3 = [1.35;1.5;1.1];
+p4 = [-1.35;1.5;1.1];
+
+f1 = [0;sin(deg2rad(30));-cos(deg2rad(30))];
+f2 = [0;sin(deg2rad(30));-cos(deg2rad(30))];
+f3 = [0;sin(deg2rad(-30));-cos(deg2rad(-30))];
+f4 = [0;sin(deg2rad(-30));-cos(deg2rad(-30))];
+
+n1 = cross(p1,f1);
+n2 = cross(p2,f2);
+n3 = cross(p3,f3);
+n4 = cross(p4,f4);
+
+%Parametros de simulação
+Dt = 0.01;
+t = 0:Dt:60;
+Nsim = length(t);
+T = (m*g_mars*0.25)/(2*cos(deg2rad(29))+2*cos(deg2rad(31)));
+px = 0;
+py = 0;
+pz = -21;
+p = [px;py;pz];
+wx = 0;  
+wy = 0;
+wz = 0;
+omega = [wx;wy;wz]; 
+vx = 0;
+vy = 4;
+vz = 0;
+v = [vx;vy;vz];
+phi = deg2rad(1);
+theta = deg2rad(0);
+psi = deg2rad(0);
+lbd = [phi;theta;psi];
+x0 = [p; v; lbd; omega];
+u = ones(Nsim,1)*[T,T,T,T];
+
+a = [0, -vx*sin(theta)+vz*cos(theta), -vx*sin(psi)-vy*cos(psi)
+     -vy*sin(phi)-vz*cos(phi), 0, vx*cos(psi)-vy*sin(psi)
+      vy*cos(phi)-vz*sin(phi), vx*cos(theta)-vz*sin(theta), 0];
+
+b = [(wy*cos(phi)-wz*sin(phi))*tan(theta), (wy*sin(phi)+wz*cos(phi))/((cos(theta))^2), 0
+     -wy*sin(phi)-wz*cos(phi), 0, 0
+      (wy*cos(phi)-wz*sin(phi))/cos(theta), ((wy*sin(phi)+wz*cos(phi))*tan(theta))/cos(theta), 0];
+
+c = [0, -g_mars, 0
+     g_mars*cos(phi), 0, 0
+     -g_mars*sin(phi), 0, 0];
+
+
+A = [ zeros(3), Euler2R(lbd), a, zeros(3)
+     zeros(3), zeros(3), c, skew(v)
+     zeros(3), zeros(3), b, Euler2Q(lbd)
+     zeros(3), zeros(3), zeros(3), zeros(3)];
+
+B=[zeros(3,4)
+   1/mt*f1, 1/mt*f2, 1/mt*f3, 1/mt*f4
+   zeros(3,4)
+   J^-1*n1, J^-1*n2, J^-1*n3, J^-1*n4];
+
+C = eye(12);
+
+D = zeros(12,4);
+
+sys = ss(A,B,C,D);
+y_L = lsim(sys,u,t,x0);
+
+figure(1);
+plot(t,y_L(:,1),'k',t,y_L(:,2),'r',t,y_L(:,3),'g',t,y_L(:,4),'y',t,y_L(:,5),'b',t,y_L(:,6),'c');
+legend('px','py','pz','vx','vy','vz')
+grid on;
+
+[V,DL,W] = eig(A); % W'*A = D*W'
+
+% Teste de estabilidade, controlabilidade e observabilidade
+[M,J] = jordan(A),
+if rank(ctrb(A,B)) < size(A,1), disp('O Sistema não é controlável'); end
+if rank(obsv(A,C)) < size(A,1), disp('O Sistema não é observável'); end
+ctrb_modes = W'*B,
+obsv_modes = C*V,
+
+%SVD
+G = tf(sys);
+%plot dos valores singulares
+figure(2);
+sigmaplot(G);
+grid on;
+title('Singular Values');
+
+%LQR
+Q = blkdiag(1,10,100,1,100,10,0.01*eye(3),0.001*eye(3));
+R = 0.1*eye(4);
+Klqr = lqr(A,B,Q,R);
+lbd_CL_lqr = eig(A-B*Klqr); 
+X = A-B*Klqr;
+CLlqr = ss(X,B,C,D);
+if any(real(lbd_CL_lqr) >= 0), disp('CL system with LQR not stable'); else, disp('CL system with LQR is stable'); end
+
+%Simulação do LQR
+Dt = 0.01;
+t = 0:Dt:100;
+NSim = length(t);
+r = [0;1000;-21;0;0;0;0;0;0;0;0;0]*(t>=0);
+nx = 12;
+nu = 4;
+x = zeros(nx,NSim);
+xu = zeros(nx,NSim);
+u = zeros(nu,NSim);
+x(:,1) = [0;0;-21;0;4;0;deg2rad(1);0;0;0;0;0];
+%C = eye(12);
+for k = 1:NSim
+
+    y(:,k) = C*x(:,k);
+
+    u(:,k) = -Klqr*[y(:,k) - r(:,k)];
+
+    x_dot = A*x(:,k) + B*u(:,k); % system derivatives
+    xp = x(:,k) + Dt*x_dot; % integrate system state
+    if k < NSim
+        x(:,k+1) = xp;
+    end
+end
+
+figure(3);
+subplot(4,1,1);
+plot(t,r(2,:),t,r(3,:),t,y(1,:),t,y(2,:),t,y(3,:));
+ylabel('Posição [m]');
+xlabel('Tempo [s]');
+legend('rp_y','rp_z','p_x','p_y','p_z');
+grid on;
+
+subplot(4,1,2);
+plot(t,r(6,:),t,y(4,:),t,y(5,:),t,y(6,:));
+ylabel('Velocidade [m/s]');
+xlabel('Tempo [s]');
+legend('r_v_z','v_x','v_y','v_z');
+grid on;
+
+subplot(4,1,3);
+plot(t,r(7,:),t,r(8,:),t,y(7,:),t,y(8,:),t,y(9,:));
+ylabel('Ângulos de Euler [rad]');
+xlabel('Tempo [s]');
+legend('r_phi','r_theta','phi','theta','psi');
+grid on;
+
+subplot(4,1,4);
+plot(t,u(1,:),t,u(2,:),t,u(3,:),t,u(4,:));
+ylabel('Thrust [N]');
+xlabel('Tempo [s]');
+legend('$$T_1$$','$$T_2$$','$$T_3$$','$$T_4$$');
+grid on;
+
+%H_inf
+B1 = zeros(12);
+B2 = B;
+W1 = sqrt(Q);
+W2 = sqrt(R);
+C1 = [W1];
+D11 = zeros(12);
+D12 = [zeros(8,4);W2];
+C2 = -eye(12);
+D21 = eye(12);
+D22 = zeros(12,4);
+
+A0 = A;
+B0 = [B1,B2];
+C0 = [C1;C2];
+D0 = [D11 D12; D21 D22];
+
+P = ss(A0,B0,C0,D0);
+
+nmeas = 12;
+ncont = 4;
+% tests on P:
+
+nx = size(A0,1);
+if (nx - rank(ctrb(A0,B2))) > 0, disp('A1.1 on P: system uncontrolable'); else disp('A1.1 on P: OK'); end
+if (nx - rank(obsv(A0,C2))) > 0, disp('A1.2 on P: system unobservable'); else disp('A1.2 on P: OK'); end
+if (size(D12,2) - rank(D12)) > 0, disp('A2.1 on P: D12 is column rank deficient'); else disp('A2.1 on P: OK'); end
+if (size(D21,1) - rank(D21)) > 0, disp('A2.2 on P: D21 is row rank deficient'); else disp('A2.1 on P: OK'); end
+syms w real; 
+Aux1 = [A0 - j*w*eye(size(A0)) , B2 ; C1 , D12];
+if (size(Aux1,2) - rank(Aux1)) > 0,  disp('A3 on P: matrix is column rank deficient'); else disp('A3 on P: OK'); end
+Aux2 = [A0 - j*w*eye(size(A0)) , B1 ; C2 , D21];
+if (size(Aux2,1) - rank(Aux2)) > 0,  disp('A4 on P: matrix is column rank deficient'); else disp('A4 on P: OK'); end
+
+[Kinf,CLinf,gammainf,info_inf] = hinfsyn(P,nmeas,ncont);
+poles_CLinf = pole(CLinf);
+if any(real(poles_CLinf) >= 0), disp('CL system with Hinf controller not stable'); else, disp('CL system with Hinf controller is stable'); end
+
+for k = 1:NSim
+
+    y(:,k) = C*x(:,k);
+
+    v = -[(r(:,k)-y(:,k))];
+    u(:,k) = Kinf.C*v;
+
+    x_dot = A*x(:,k) + B*u(:,k); 
+    xp = x(:,k) + Dt*x_dot; 
+    if k < NSim
+        x(:,k+1) = xp;
+    end
+end
+
+figure(4);
+subplot(4,1,1);
+plot(t,r(2,:),t,r(3,:),t,y(1,:),t,y(2,:),t,y(3,:));
+ylabel('Posição [m]');
+xlabel('Tempo [s]');
+legend('rp_y','rp_z','p_x','p_y','p_z');
+grid on;
+
+subplot(4,1,2);
+plot(t,r(6,:),t,y(4,:),t,y(5,:),t,y(6,:));
+ylabel('Velocidade [m/s]');
+xlabel('Tempo [s]');
+legend('r_v_z','v_x','v_y','v_z');
+grid on;
+
+subplot(4,1,3);
+plot(t,r(7,:),t,r(8,:),t,y(7,:),t,y(8,:),t,y(9,:));
+ylabel('Ângulos de Euler [rad]');
+xlabel('Tempo [s]');
+legend('r_phi','r_theta','phi','theta','psi');
+grid on;
+
+subplot(4,1,4);
+plot(t,u(1,:),t,u(2,:),t,u(3,:),t,u(4,:));
+ylabel('Thrust [N]');
+xlabel('Tempo [s]');
+legend('T_1','T_2','T_3','T_4');
+grid on;
+
+figure(5);
+plot(t,r(2,:),t,r(3,:),t,y(1,:),t,y(2,:),t,y(3,:));
+ylabel('Posição [m]');
+xlabel('Tempo [s]');
+legend('rp_y','rp_z','p_x','p_y','p_z');
+grid on;
+
+figure(6);
+sigmaplot(CLlqr,'k',Kinf);
+grid on;
+title('Sigma Values - CL (no weights)');
+legend('LQR','H\infty');
